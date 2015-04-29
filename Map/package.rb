32119421fb90e8583172b684b4f6c8b74e265994
vincent26 <zeludtnecniv@gmail.com>
2015-04-29 23:47:36 +0200

    Update package.rb

--- Map/package.rb
+++ Map/package.rb
@@ -3,6 +3,6 @@ Package.new(
 	name: 'Floor_Creator',
 	version: vsn(0, 2, 3),
 	authors: {'Vincent26' => 'zeludtnecniv@gmail.com'},
-	components: ['script.rb'],
+	components: ['Etage.rb'],
 	description: 'Offer the management of floor easily'
 )

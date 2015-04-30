023d5e7a262ff3c24f551de48a9829a4eebc765c
vincent26 <zeludtnecniv@gmail.com>
2015-04-30 14:44:50 +0200

    Update package.rb

--- Utilitaire/Aide_overlay_mapping/package.rb
+++ Utilitaire/Aide_overlay_mapping/package.rb
@@ -1,7 +1,7 @@
 # Package description
 Package.new(
 	name: 'Overlay_Helper',
-	version: vsn(0, 2, 3),
+	version: vsn(0, 1, 0),
 	authors: {'Vincent26' => 'zeludtnecniv@gmail.com'},
 	components: ['Aide_overlay_mapping.rb'],
 	description: 'Creator tools for help with overlay mapping'

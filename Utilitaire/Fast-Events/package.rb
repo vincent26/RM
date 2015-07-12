59cd38363af42f341e88bccd8dc807514b5eaec3
vincent26 <zeludtnecniv@gmail.com>
2015-07-12 15:16:20 +0200

    Create package.rb

---
+++ Utilitaire/Fast-Events/package.rb
@@ -0,0 +1,8 @@
+# Package description
+Package.new(
+	name: 'Fast_Events',
+	version: vsn(0, 1, 0),
+	authors: {'Vincent26' => 'zeludtnecniv@gmail.com'},
+	components: ['Fast_Events.rb'],
+	description: 'Creator tools for events making'
+)

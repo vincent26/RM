b321c8f37011a383ed47e4c9e74b8f24dc9182aa
vincent26 <zeludtnecniv@gmail.com>
2015-08-22 23:47:35 +0200

    Update package.rb

--- Menu/Limiteur/package.rb
+++ Menu/Limiteur/package.rb
@@ -1,7 +1,7 @@
 # Package description
 Package.new(
 	name: 'Limiter_inventory',
-	version: vsn(0, 1, 1),
+	version: vsn(0, 1, 2),
 	authors: {'Vincent26' => 'zeludtnecniv@gmail.com'},
 	components: ['Limiteur.rb'],
 	description: 'Create a limitation of items than the character can transport'

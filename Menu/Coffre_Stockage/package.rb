cfd29bec38b14109cb10c0d63b91dec441ff974f
vincent26 <zeludtnecniv@gmail.com>
2015-08-22 23:42:51 +0200

    Update package.rb

--- Menu/Coffre_Stockage/package.rb
+++ Menu/Coffre_Stockage/package.rb
@@ -1,7 +1,7 @@
 # Package description
 Package.new(
 	name: 'Storage_Chest',
-	version: vsn(0, 1, 0),
+	version: vsn(0, 1, 1),
 	authors: {'Vincent26' => 'zeludtnecniv@gmail.com'},
 	components: ['Coffre_Stockage.rb'],
 	description: 'Add the possibility to make storage chest in game'
